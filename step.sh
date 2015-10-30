5d9c4f05ef499650df8701bf984ee0da733f79b6
birmacher <birmacher@gmail.com>
2015-10-30 22:24:27 +0000

    support for separate gradle_file

--- step.sh
+++ step.sh
@@ -2,10 +2,15 @@
 
 set -e
 
-if [ -z "${gradle_task}" ]; then
-	printf "\e[31mNo gradle task found\e[0m\n"
+if [ -z "${gradle_file}" ]; then
+	printf "\e[31mNo gradle_file specified\e[0m\n"
 	exit 1
 fi
 
-echo "$" gradle ${gradle_task}
-gradle ${gradle_task}
+if [ -z "${gradle_task}" ]; then
+	printf "\e[31mNo gradle_task specified\e[0m\n"
+	exit 1
+fi
+
+echo "$" gradle --build-file "${gradle_file}" ${gradle_task}
+gradle --build-file "${gradle_file}" ${gradle_task}
