3e96ed65730b8bf38c1c27fc4ca4af9ea1e8b71a
birmacher <birmacher@gmail.com>
2015-10-30 23:34:52 +0000

    Copies all or the specified APKs to $BITRISE_DEPLOY_DIR

--- step.sh
+++ step.sh
@@ -12,5 +12,18 @@ if [ -z "${gradle_task}" ]; then
 	exit 1
 fi
 
+if [ -z "${apk_file_filter}" ]; then
+	export apk_file_filter="*.apk"
+fi
+
 echo "$" gradle --build-file "${gradle_file}" ${gradle_task}
 gradle --build-file "${gradle_file}" ${gradle_task}
+
+echo ""
+echo "Moving APK files with filter: ${apk_file_filter}"
+find . -name "${apk_file_filter}" | while IFS= read -r apk; do 
+	export deploy_path="${BITRISE_DEPLOY_DIR}/$(echo "$apk" | rev | cut -d"/" -f1 | rev)"
+	
+	printf "🚀  \e[32mCopy ${apk} to ${deploy_path}\e[0m\n"
+	cp "${apk}" "${deploy_path}"
+done
