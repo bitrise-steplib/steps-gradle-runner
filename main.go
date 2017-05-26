457d16132fb21435ca43e488f1b9dc7a37e46a46
Tamas Papik <trapacska@gmail.com>
2017-05-26 14:33:21 +0200

    Added built-in cache collection (#30)
    * initial
    
    * Update main.go
    
    * fix
    
    * fix
    
    * fix
    
    * clean-up
    
    * added logs
    
    * log clean-up
    
    * gitignore change
    
    * removed gows files
    
    * added --no-daemon flag

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"github.com/bitrise-io/go-utils/log"
 	"github.com/bitrise-io/go-utils/pathutil"
 	"github.com/bitrise-io/go-utils/retry"
+	"github.com/bitrise-tools/go-steputils/cache"
 	"github.com/kballard/go-shellquote"
 )
 
@@ -222,7 +223,61 @@ func main() {
 		failf("Gradle task failed, error: %s", err)
 	}
 
+	// Collecting caches
+	fmt.Println()
+	log.Infof("Collecting gradle caches...")
+
+	gradleCache := cache.New()
+	homeDir := pathutil.UserHomeDir()
+
+	includePths := []string{
+		filepath.Join(homeDir, ".gradle"),
+		filepath.Join(homeDir, ".kotlin"),
+		filepath.Join(homeDir, ".android", "build-cache"),
+	}
+	excludePths := []string{
+		"/*.lock",
+		"/*.bin",
+		"/*/build/*.json",
+		"/*/build/*.xml",
+		"/*/build/*.properties",
+		"/*/build/*/zip-cache/*",
+		"/*.log",
+		"/*.txt",
+		"/*.rawproto",
+		"/*.ap_",
+		"/*.apk",
+	}
+
+	projectRoot, err := filepath.Abs(filepath.Dir(configs.GradlewPath))
+	if err != nil {
+		log.Warnf("Cache collection skipped: failed to determine project root path.")
+	} else {
+		if err := filepath.Walk(projectRoot, func(path string, f os.FileInfo, err error) error {
+			if f.IsDir() {
+				if f.Name() == "build" {
+					includePths = append(includePths, path)
+				}
+				if f.Name() == ".gradle" {
+					includePths = append(includePths, path)
+				}
+			}
+			return nil
+		}); err != nil {
+			log.Warnf("Cache collection skipped: failed to determine cache paths.")
+		} else {
+
+			gradleCache.IncludePath(strings.Join(includePths, "\n"))
+			gradleCache.ExcludePath(strings.Join(excludePths, "\n"))
+
+			if err := gradleCache.Commit(); err != nil {
+				log.Warnf("Cache collection skipped: failed to commit cache paths.")
+			}
+		}
+	}
+
 	// Move apk files
+	fmt.Println()
 	log.Infof("Move apk files...")
 	apkFiles, err := find(".", configs.ApkFileIncludeFilter, configs.ApkFileExcludeFilter)
 	if err != nil {
