fb5c46f9cd4bf7cb6177ad83477ae72710be277c
Laszlo Szucs <laszlo.szucs@bitrise.io>
2019-10-28 17:34:58 +0100

    vendor folder

---
+++ vendor/github.com/bitrise-steplib/bitrise-step-android-unit-test/cache/cache.go
@@ -0,0 +1,126 @@
+package cache
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/bitrise-io/go-steputils/cache"
+	"github.com/bitrise-io/go-utils/fileutil"
+	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/go-utils/pathutil"
+)
+
+// Level ...
+type Level string
+
+// Cache level
+const (
+	LevelNone = Level("none")
+	LevelDeps = Level("only_deps")
+	LevelAll  = Level("all")
+)
+
+// Collect ...
+func Collect(projectRoot string, cacheLevel Level) error {
+	if cacheLevel != LevelNone {
+		gradleCache := cache.New()
+		homeDir := pathutil.UserHomeDir()
+
+		projectRoot, err := filepath.Abs(projectRoot)
+		if err != nil {
+			return fmt.Errorf("cache collection skipped: failed to determine project root path")
+		}
+
+		lockFilePath := filepath.Join(projectRoot, "gradle.deps")
+
+		excludePths := []string{
+			"~/.gradle/**",
+			"~/.android/build-cache/**",
+			"*.lock",
+			"*.bin",
+			"*/build/*.json",
+			"*/build/*.html",
+			"*/build/*.xml",
+			"*/build/*.properties",
+			"*/build/*/zip-cache/*",
+			"*.log",
+			"*.txt",
+			"*.rawproto",
+			"!*.ap_",
+			"!*.apk",
+		}
+
+		var includePths []string
+
+		if cacheLevel == LevelAll || cacheLevel == LevelDeps {
+			lockfileContent := ""
+			if err := filepath.Walk(projectRoot, func(path string, f os.FileInfo, err error) error {
+				if !f.IsDir() && strings.HasSuffix(f.Name(), ".gradle") && !strings.Contains(path, "node_modules") {
+					if md5Hash, err := computeMD5String(path); err != nil {
+						log.Warnf("Failed to compute MD5 hash of file(%s), error: %s", path, err)
+					} else {
+						lockfileContent += md5Hash
+					}
+				}
+				return nil
+			}); err != nil {
+				return fmt.Errorf("dependency map generation skipped: failed to collect dependencies")
+			}
+			if err := fileutil.WriteStringToFile(lockFilePath, lockfileContent); err != nil {
+				return fmt.Errorf("dependency map generation skipped: failed to write lockfile, error: %s", err)
+			}
+
+			includePths = append(includePths, fmt.Sprintf("%s -> %s", filepath.Join(homeDir, ".gradle"), lockFilePath))
+			includePths = append(includePths, fmt.Sprintf("%s -> %s", filepath.Join(homeDir, ".kotlin"), lockFilePath))
+			includePths = append(includePths, fmt.Sprintf("%s -> %s", filepath.Join(homeDir, ".m2"), lockFilePath))
+		}
+
+		if cacheLevel == LevelAll {
+			includePths = append(includePths, fmt.Sprintf("%s -> %s", filepath.Join(homeDir, ".android", "build-cache"), lockFilePath))
+
+			if err := filepath.Walk(projectRoot, func(path string, f os.FileInfo, err error) error {
+				if f.IsDir() {
+					if f.Name() == "build" {
+						includePths = append(includePths, path)
+					}
+					if f.Name() == ".gradle" {
+						includePths = append(includePths, path)
+					}
+				}
+				return nil
+			}); err != nil {
+				return fmt.Errorf("cache collection skipped: failed to determine cache paths")
+			}
+		}
+		gradleCache.IncludePath(strings.Join(includePths, "\n"))
+		gradleCache.ExcludePath(strings.Join(excludePths, "\n"))
+
+		if err := gradleCache.Commit(); err != nil {
+			return fmt.Errorf("cache collection skipped: failed to commit cache paths")
+		}
+	}
+	return nil
+}
+
+func computeMD5String(filePath string) (string, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Errorf("Failed to close file(%s), error: %s", filePath, err)
+		}
+	}()
+
+	h := md5.New()
+	if _, err := io.Copy(h, f); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", h.Sum(nil)), nil
+}
